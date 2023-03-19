c10da67a6625566dd147a3098eb71ac9b2e34c21
Andreas Wissel <business@andreaswissel.com>
2023-03-19 23:37:36 +0100

    lab-8

--- design-systems/src/app/button/button.module.ts
+++ design-systems/src/app/button/button.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button.component';
-
-
+import { IconModule } from '../icon/icon.module';
 
 @NgModule({
-  declarations: [
-    ButtonComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ButtonComponent],
+  imports: [CommonModule, IconModule],
+  exports: [ButtonComponent],
 })
-export class ButtonModule { }
+export class ButtonModule {}
